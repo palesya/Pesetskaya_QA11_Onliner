741c771d8d2c7da471968890e65fb08a98604f5a
”Alesya <palesyav89@gmail.com>
2022-05-02 15:35:33 +0300

    RestAssured helper added

--- src/main/java/Utilities/RestAssuredHelper.java
+++ src/main/java/Utilities/RestAssuredHelper.java
@@ -1,6 +1,33 @@
 package Utilities;
 
-public class RestAssuredHelper {
+import com.google.gson.Gson;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
+import static io.restassured.RestAssured.given;
+
+public abstract class RestAssuredHelper {
+
+    protected JsonPath getJsonPathFromGetRequest(String uri) {
+        Response responseCatalog = given().get(uri);
+        checkStatusCodeOK(responseCatalog);
+        return responseCatalog.jsonPath();
+    }
+
+    protected String getFirstValueFromJsonPath(JsonPath jsonPath, String stringPath) {
+        return jsonPath.getList(stringPath).get(0).toString();
+    }
+
+    protected JsonPath getJsonPathFromPutRequest(String uri, Object object) {
+        Gson gson = new Gson();
+        String objectString = gson.toJson(object);
+        Response responseCart = given().when().header("Content-Type", "application/json").and().body(objectString).post(uri);
+        checkStatusCodeOK(responseCart);
+        return responseCart.jsonPath();
+    }
+
+    private void checkStatusCodeOK(Response response) {
+        response.then().assertThat().statusCode(200);
+    }
 
 }
--- src/test/java/OnlinerTests/APITestsRestAssured.java
+++ src/test/java/OnlinerTests/APITestsRestAssured.java
@@ -1,17 +1,15 @@
 package OnlinerTests;
 
 import Entity.Cart;
-import com.google.gson.Gson;
+import Utilities.RestAssuredHelper;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
 
-public class APITestsRestAssured {
+public class APITestsRestAssured extends RestAssuredHelper {
 
     @BeforeTest
     public void precondition() {
@@ -21,33 +19,23 @@ public class APITestsRestAssured {
     @Test(priority = 1)
     public void onlinerCatalog_Test() {
         //catalog
-        Response responseCatalog = given().get("/catalog.api/search/mobile?on_sale=1&mfr[0]=apple&group=1");
-        responseCatalog.then().assertThat().statusCode(200);
-        JsonPath jsonPathCatalog = responseCatalog.jsonPath();
-        String productID= jsonPathCatalog.getList("products.id").get(0).toString();
-        String productKey = jsonPathCatalog.getList("products.key").get(0).toString();
-        String productName = jsonPathCatalog.getList("products.name").get(0).toString();
+        JsonPath jsonPathCatalog = getJsonPathFromGetRequest("/catalog.api/search/mobile?on_sale=1&mfr[0]=apple&group=1");
+        String productID= getFirstValueFromJsonPath(jsonPathCatalog,"products.id");
+        String productKey = getFirstValueFromJsonPath(jsonPathCatalog,"products.key");
+        String productName = getFirstValueFromJsonPath(jsonPathCatalog,"products.name");
 
         //position
-        Response responsePosition = given().get("/shop.api/products/"+ productKey + "/positions");
-        responsePosition.then().assertThat().statusCode(200);
-        JsonPath jsonPathPosition = responsePosition.jsonPath();
-        String positionID= jsonPathPosition.getList("positions.primary.id").get(0).toString();
-        String shopID = jsonPathPosition.getList("positions.primary.shop_id").get(0).toString();
+        JsonPath jsonPathPosition = getJsonPathFromGetRequest("/shop.api/products/"+ productKey + "/positions");
+        String positionID= getFirstValueFromJsonPath(jsonPathPosition,"positions.primary.id");
+        String shopID = getFirstValueFromJsonPath(jsonPathPosition,"positions.primary.shop_id");
 
         //cartID
         Cart cartBody = Cart.builder().position_id(positionID).product_id(productID).shop_id(shopID).quantity(1).product_key(productKey).build();
-        Gson gson = new Gson();
-        String cartBodyString = gson.toJson(cartBody);
-        Response responseCart = given().when().header("Content-Type", "application/json").and().body(cartBodyString).post("/cart.api/detached-cart/add/");
-        responseCart.then().assertThat().statusCode(200);
-        JsonPath jsonPathCart = responseCart.jsonPath();
+        JsonPath jsonPathCart = getJsonPathFromPutRequest("/cart.api/detached-cart/add/",cartBody);
         String cartID = jsonPathCart.get("cart_id").toString();
 
         //Get cart
-        Response responseGetCart = given().get("https://catalog.onliner.by/sdapi/cart.api/v2/detached-cart/" + cartID);
-        responseGetCart.then().assertThat().statusCode(200);
-        JsonPath jsonPathGetCart = responseGetCart.jsonPath();
+        JsonPath jsonPathGetCart = getJsonPathFromGetRequest("https://catalog.onliner.by/sdapi/cart.api/v2/detached-cart/"+ cartID);
         String actualProductName=jsonPathGetCart.get("position_groups.positions.product.name").toString();
         Assert.assertTrue(actualProductName.contains(productName));
     }
